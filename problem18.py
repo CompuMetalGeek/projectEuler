616ba02d5388df2b4c2c1e2a082a0d3c65fc22fb
Jarno De Meyer <jarno.de.meyer@gmail.com>
2017-12-15 15:45:51 +0100

    Solved problems 18 and 67

---
+++ problem18.py
@@ -0,0 +1,25 @@
+import sys
+
+def reduceOneLevel():
+	level = len(piramid)-1
+	rowtoRemove = piramid.pop(level)
+	for i in range(len(piramid[level-1])) :
+		piramid[level-1][i]+=max(rowtoRemove[i],rowtoRemove[i+1])
+
+input = open("problem18.py_input","r")
+
+piramid = dict();
+i=0
+for line in input :
+	piramid[i] = list(map(int,line.split(" ")))
+	i+=1
+
+
+print("---",len(piramid),"---")
+for i in range(len(piramid)):
+	print(piramid[i])
+while len(piramid)>1 :
+	reduceOneLevel()
+	print("---",len(piramid),"---")
+	for i in range(len(piramid)):
+		print(piramid[i])
